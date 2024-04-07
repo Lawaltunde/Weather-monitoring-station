614a50abcd70f46cd169f849f4a419ce8e09801e
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-04-07 17:11:24 +0000

    ForecastDisplay Observer created, this is yet another observer that displays a weather forecast based on the current weather data

--- WeatherStation/Weather/CurrentConditionsDisplay.cs
+++ WeatherStation/Weather/CurrentConditionsDisplay.cs
@@ -3,34 +3,36 @@ using Weather.Interfaces;
 
 public class CurrentConditionsDisplay : IDisplay<string>
 {
+    private double output;
     private DateTime dateTime = new DateTime();
-    public  void Display(string parameter){
+    public  void Display(string param){
         Console.WriteLine("Date: "+ dateTime.Date);
         Console.WriteLine("Time: "+ dateTime.TimeOfDay);
 
-        double val = double.Parse(parameter);
-
-        switch(val){
+        if(double.TryParse(param, out output))
+        {
+            switch(output){
             case <= -20.0: 
-                Console.WriteLine("{0}°C: Extreme Cold", parameter);
+                Console.WriteLine("{0}°C: Extreme Cold", output);
                 break;
 
             case <= -5.0: 
-                 Console.WriteLine("{0}°C: Cold", parameter);
+                 Console.WriteLine("{0}°C: Cold", output);
                  break;
             case <= 10.0: 
-                 Console.WriteLine("{0}°C: Cool", parameter);
+                 Console.WriteLine("{0}°C: Cool", output);
                  break;
             case <= 25.0: 
-                 Console.WriteLine("{0}°C: Mild", parameter);
+                 Console.WriteLine("{0}°C: Mild", output);
                  break;
 
             case <=35.0: 
-                 Console.WriteLine("{0}°C: Walm", parameter);
+                 Console.WriteLine("{0}°C: Walm", output);
                  break;
             default:
                 throw new Exception("wrong value inserted");
         };
+        }
 
     }
 
---
+++ WeatherStation/Weather/ForecastDisplay.cs
@@ -0,0 +1,32 @@
+using WeatherStation.Weather.Interfaces;
+
+public class ForecastDisplay : IDisplay<string>
+{
+    private double output;
+    public void Display(String param){
+        if(double.TryParse(param, out output))
+        {
+            switch(output){
+            case <= -20.0: 
+                Console.WriteLine("{0}°C: Rain is promised! ", output);
+                break;
+
+            case <= -5.0: 
+                 Console.WriteLine("{0}°C: Rain is promised! ", output);
+                 break;
+            case <= 10.0: 
+                 Console.WriteLine("{0}°C: Sunny with partly cloudy! ", output);
+                 break;
+            case <= 25.0: 
+                 Console.WriteLine("{0}°C: Sunny day all week! ", output);
+                 break;
+
+            case <=35.0: 
+                 Console.WriteLine("{0}°C: Walm! ", output);
+                 break;
+            default:
+                throw new Exception("wrong value inserted");
+        };
+        }
+    }
+}
