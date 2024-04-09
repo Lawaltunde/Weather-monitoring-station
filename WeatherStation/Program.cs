a4894761fd77bc8b106d3f586afe9e361070266b
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-04-09 11:22:42 -0400

    Update Program.cs

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Please enter your command (CurrentConditionsDisplay, Statisti
 
     var obs1 = Console.ReadLine()?? "";
     weatherData.Subscribe(WeatherStation_.CreateDisplay(obs1));
+Console.WriteLine("Enter temperature (This must be a figure): ");
     weatherData.Value = Console.ReadLine()?? "";
     //weatherData.Subscribe(obs1);
 
